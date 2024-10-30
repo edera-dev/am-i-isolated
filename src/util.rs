3b4855d095cceb78e129e6fb9c4cc7e953e87441
Ariadne Conill <ariadne@ariadne.space>
2024-10-30 11:42:34 -0700

    chore: util: add /proc tuple parser

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::path::Path;
 
@@ -7,6 +8,24 @@ pub fn read_file_as_lines<P: AsRef<Path>>(p: P) -> Result<Vec<String>, ()> {
             .lines()
             .map(String::from)
             .collect())
+    }
+
+    Err(())
+}
+
+pub fn read_file_as_tuples<P: AsRef<Path>>(p: P) -> Result<HashMap<String, String>, ()> {
+    if let Ok(lines) = read_file_as_lines(p) {
+        let mut map = HashMap::new();
+
+        for line in lines {
+            let fields: Vec<_> = line.split(':').collect();
+            let key = String::from(fields[0]);
+            let value = String::from(fields[1]);
+
+            map.insert(key, String::from(value.trim()));
+        }
+
+        return Ok(map)
     }
 
     Err(())
